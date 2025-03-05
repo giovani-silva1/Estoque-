044135c69fab928574e7b2700e1815cb1af1d1a5
Giovani Silva <giovani-silva1@live.com>
2025-03-04 22:40:37 -0300

    Usando e mapeado os campos para Autenticar usuários ou cadastrar novos

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  createAccountForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('DADOS DO FORMULARIO DE LOGIN', this.loginForm.value);
+  }
+
+  onSubmitCreateAccount(): void {
+    console.log(
+      'DADOS DE CRIAÇÃO DE CONTA REALIZADO',
+      this.createAccountForm.value
+    );
+  }
 }
